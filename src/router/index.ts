193c60da511d06feca2020c437c7fba8ec82bf24
nigulasis <104707844+nigulasis@users.noreply.github.com>
2022-07-23 13:07:59 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,45 +9,46 @@ const routes: Array<RouteRecordRaw> = [
     name: 'home',
     component:HomeView,
     redirect:"order",
+    children:[],
 
-    children:[
-      {
-        path:'order',
-        name:'order',
-        meta:{
-          isShow:true,
-          title:"订单列表"
-        },
-        component:() => import(/* webpackChunkName: "order" */ '../views/OrderView.vue')
-      },
-      {
-        path:'userList',
-        name:'userList',
-        meta:{
-          isShow:true,
-          title:"用户列表"
-        },
-        component:() => import(/* webpackChunkName: "userList" */ '../views/UserView.vue')
-      },
-      {
-        path:'roleList',
-        name:'roleList',
-        meta:{
-          isShow:true,
-          title:"角色列表"
-        },
-        component:() => import(/* webpackChunkName: "roleList" */ '../views/RoleView.vue') 
-      },
-      {
-        path:'authority',
-        name:'authority',
-        meta:{
-          isShow:false,
-          title:"权限列表"
-        },
-        component:() => import(/* webpackChunkName: "authorityList" */ '../views/AuthorityView.vue')
-      }
-    ]
+//     children:[
+//       {
+//         path:'order',
+//         name:'order',
+//         meta:{
+//           isShow:true,
+//           title:"订单列表"
+//         },
+//         component:() => import(/* webpackChunkName: "order" */ '../views/OrderView.vue')
+//       },
+//       {
+//         path:'userList',
+//         name:'userList',
+//         meta:{
+//           isShow:true,
+//           title:"用户列表"
+//         },
+//         component:() => import(/* webpackChunkName: "userList" */ '../views/UserView.vue')
+//       },
+//       {
+//         path:'roleList',
+//         name:'roleList',
+//         meta:{
+//           isShow:true,
+//           title:"角色列表"
+//         },
+//         component:() => import(/* webpackChunkName: "roleList" */ '../views/RoleView.vue') 
+//       },
+//       {
+//         path:'authority',
+//         name:'authority',
+//         meta:{
+//           isShow:false,
+//           title:"权限列表"
+//         },
+//         component:() => import(/* webpackChunkName: "authorityList" */ '../views/AuthorityView.vue')
+//       }
+//     ]
   },
   {
     path: '/login',
@@ -61,34 +62,33 @@ const router = createRouter({
   routes
 })
 
-//路由拦截
-//有点小问题，初次加载，子组件为空
-// router.beforeEach(async (to:any)=>{
-//   const token:string | null =localStorage.getItem('token')
-//   if(!token && to.path !="/login"){
+
+router.beforeEach(async (to:any)=>{
+  const token:string | null =localStorage.getItem('token')
+  if(!token && to.path !="/login"){
   
-//     return "login"
-//   }else if(token && to.path !="/login"){
-//     if(router.getRoutes().length==2){
-//       //添加路由
-//       let routerData:any =await getRouterList()
-//       routerData =routerData.data
-//       console.log(routerData);
+    return "login"
+  }else if(token && to.path !="/login"){
+    if(router.getRoutes().length==2){
+      //添加路由
+      let routerData:any =await getRouterList()
+      routerData =routerData.data
+      console.log(routerData);
       
-//       routerData.forEach((v:any)=>{
-//         const routerObj:RouteRecordRaw={
-//           path:v.path,
-//           name:v.name,
-//           meta:v.meta,
-//           component: () => import(/* webpackChunkName: "[request]" */ `../views/${v.path}.vue`)
-//         }
-//         router.addRoute('home',routerObj)
-//       })
-//       router.replace(to.path)
+      routerData.forEach((v:any)=>{
+        const routerObj:RouteRecordRaw={
+          path:v.name,
+          name:v.name,
+          meta:v.meta,
+          component: () => import(/* webpackChunkName: "[request]" */ `../views/${v.path}.vue`)
+        }
+        router.addRoute('home',routerObj)
+      })
+      router.replace(to.path)
       
-//     }
-//   }else if(token && to.path =="/login" )
-//     return '/OrderView'
-// })
+    }
+  }else if(token && to.path =="/login" )
+    return '/OrderView'
+})
 
 export default router
